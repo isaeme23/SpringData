aa753fc4116d8fe88f0ebdcee7ead6718b329792
unknown <isabella.manrique@labinfo.is.escuelaing.edu.co>
2023-09-07 07:49:38 -0500

    Login allowed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+CREATE TABLE countries (
+    id INT NOT NULL,
+    name VARCHAR(10)
+);
 INSERT INTO countries (id, name) VALUES (1, 'USA');
 INSERT INTO countries (id, name) VALUES (2, 'France');
 INSERT INTO countries (id, name) VALUES (3, 'Brazil');
